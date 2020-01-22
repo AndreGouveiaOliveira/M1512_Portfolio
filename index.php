3a509f798c4986e0bbeee22790ca155a3708b212
Andre <andre.gvdlv@eduge.ch>
2020-01-22 16:10:43 +0100

    skellletton first

---
+++ index.php
@@ -0,0 +1,52 @@
+<nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" href="index.php">Mon Blog</a>
+
+        <ul class="uk-navbar-nav uk-position-right">
+            <li>
+                <a class="uk-navbar-item uk-logo" href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Features
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+<div class="uk-flex uk-margin-small-left">
+    <div class="uk-margin-medium-left">
+        <div class="uk-card uk-card-default uk-margin-medium-top">
+            <div class="uk-card-media-top">
+                <img src="images/nature.jpg" height="520" width="580" alt="">
+            </div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">Nom du Blog</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="uk-card uk-card-default uk-card-body uk-margin-medium-top">
+            <h3 class="uk-card-title">A propos</h3>
+            <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom">Je test1</button>
+            <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom">Je test2</button>
+            <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom">Je test3</button>
+        </div>
+    </div>
+
+    <div class="uk-margin-medium-left uk-margin-medium-right uk-width-3-4">
+        <div class="uk-card uk-card-default uk-card-body uk-margin-medium-top">
+            <h3 class="uk-card-title">Welcome</h3>
+        </div>
+        <div class="uk-card uk-card-default uk-card-body uk-margin-medium-top">
+            <h3 class="uk-card-title">Salut poto</h3>
+        </div>
+    </div>
+</div>
+
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="js/uikit.min.js"></script>
+<script src="js/uikit-icons.min.js"></script>
