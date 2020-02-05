8ac8bc00e812bc8277d7693cdccbb739218e4c98
Andre <andre.gvdlv@eduge.ch>
2020-02-05 13:58:36 +0100

    finish squelete

--- post.php
+++ post.php
@@ -11,14 +11,15 @@
                 </a>
             </li>
             <li>
-                <a class="uk-navbar-item uk-logo" href="post.php">
+                <a class="uk-navbar-item uk-logo" href="">
                     <span class="uk-icon uk-margin-small-right" uk-icon="icon: plus"></span>
                     New Post
                 </a>
             </li>
             <li>
-                <a class="uk-navbar-item uk-logo" href="index.php">
+                <a class="uk-navbar-item uk-logo" href="">
                     <span class="uk-icon uk-margin-small-right" uk-icon="icon: user"></span>
+                    Login
                 </a>
             </li>
         </ul>
@@ -49,36 +50,37 @@
         <div class="uk-margin-medium-left uk-margin-medium-right uk-width-3-4">
 
             <div class="uk-card uk-card-default uk-card-body uk-margin-medium-top">
-                <div class="uk-card-header">
-                    <div class="uk-grid-small uk-flex-middle" uk-grid>
-                        <div class="uk-width-auto">
-                            <img class="uk-border-circle" width="40" height="40" src="images/icon.jpg">
-                        </div>
-                        <div class="uk-width-expand">
-                            <h3 class="uk-card-title uk-margin-remove-bottom">Welcome</h3>
+                <form>
+
+                    <legend class="uk-legend">
+                        Nouveau post
+                    </legend>
+                    <div class="uk-width-auto uk-position-top-right uk-margin-medium-right uk-margin-medium-top">
+                        <img class="uk-border-circle" width="40" height="40" src="images/icon.jpg">
+                    </div>
+
+                    <div class="uk-margin">
+                        <input class="uk-input" type="text" placeholder="Input">
+                    </div>
+
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+                    </div>
+
+                    <div class="uk-margin">
+                        <p class="uk-margin">
+                            <button class="uk-button uk-button-default">Publier</button>
+                        </p>
+                    </div>
+
+                    <div class="uk-margin uk-position-bottom-right uk-margin-medium-right">
+                        <div uk-form-custom>
+                            <input type="file">
+                            <span class="uk-text-middle">Image</span>
+                            <span class="uk-link uk-icon" uk-icon="icon: plus-circle"></span>
                         </div>
                     </div>
-                </div>
-                <div class="uk-card-body">
-                    <p>Good to see you</p>
-                </div>
-            </div>
-
-
-            <div class="uk-card uk-card-default uk-card-body uk-margin-medium-top">
-                <div class="uk-card-header">
-                    <div class="uk-grid-small uk-flex-middle" uk-grid>
-                        <div class="uk-width-auto">
-                            <img class="uk-border-circle" width="40" height="40" src="images/icon.jpg">
-                        </div>
-                        <div class="uk-width-expand">
-                            <h3 class="uk-card-title uk-margin-remove-bottom">Salut Poto</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="uk-card-body">
-                    <p>Comment va</p>
-                </div>
+                </form>
             </div>
         </div>
     </div>
