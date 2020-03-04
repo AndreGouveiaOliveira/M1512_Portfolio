e4998b8dfb4e72db30d85bb075d6abeb98da52d9
Andre <andre.gvdlv@eduge.ch>
2020-03-04 13:46:57 +0100

    change name file

--- upload.php
+++ upload.php
@@ -1,16 +1,39 @@
 <?php
-if(isset($_FILES['img']))
-{ 
-     $dossier = 'upload/';
-     $fichier = basename($_FILES['img']['name']);
-     if(move_uploaded_file($_FILES['img']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+if (isset($_FILES['img'])) {
+    $dossier = 'upload/';
+    $fichier = basename($_FILES['img']['name']);
+    $dir = "upload";
+    $nbFile = 0;
+
+    $fichier = $nbFile . $_FILES['img']['name'];
+    
+
+    // Open a directory, and read its contents
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                echo "filename:" . $file . "<br>";
+                if ($file == $fichier) {
+                    $nbFile++;
+                    $fichier = $nbFile . $_FILES['img']['name'];
+                }
+            }
+            closedir($dh);
+        }
+    }
+
+    $_FILES['img']['name'] = $fichier;
+
+    move_uploaded_file($_FILES['img']['tmp_name'], $dossier . $fichier);
+
+    /*if(move_uploaded_file($_FILES['img']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
      {
           echo 'Upload effectué avec succès !';
      }
      else //Sinon (la fonction renvoie FALSE).
      {
           echo 'Echec de l\'upload !';
-     }
+     }*/
 }
 
 header("Location: post.php");
